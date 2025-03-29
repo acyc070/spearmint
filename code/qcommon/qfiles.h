3e21dfc71114d385e39b101f9ed6ecc8965a76cb
acyc070 <acyc070@gmail.com>
2025-03-30 00:50:06 +0400

    Update qfiles.h

--- code/qcommon/qfiles.h
+++ code/qcommon/qfiles.h
@@ -43,7 +43,7 @@ Suite 120, Rockville, Maryland 20850 USA.
 #endif
 
 // surface geometry should not exceed these limits
-#define	SHADER_MAX_VERTEXES	1201 // 1200 + 1 buffer for RB_EndSurface overflow check
+#define	SHADER_MAX_VERTEXES	8193 // 1200 + 1 buffer for RB_EndSurface overflow check
 #define	SHADER_MAX_INDEXES	(6*SHADER_MAX_VERTEXES)
 #define	SHADER_MAX_TRIANGLES	(SHADER_MAX_INDEXES/3)
 
